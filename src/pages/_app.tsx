9181828b2633db9cc234538989d4d4d71738c402
bebeji-nappa <masadecade@gmail.com>
2023-10-11 00:36:29 +0900

    [ci skip] NextAuth で認証

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,16 @@
-import React, { useCallback, useEffect } from 'react';
 import '@/styles/reset.css';
 import type { AppType } from 'next/app';
 import { trpc } from  '../utils/trpc';
-import { supabase } from '@/libs/supabase';
-import Router from "next/router";
+import { SessionProvider } from "next-auth/react"
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  const checkSession = useCallback(async () => {
-    const { data, error } = await supabase.auth.getSession();
-    if (error) return null;
-    if (!data?.session) {
-      Router.push('/sign_in');
-    }
-  }, []);
-
-  useEffect(() => {
-    checkSession();
-    supabase.auth.onAuthStateChange((event) => {
-      if (event === 'SIGNED_IN') {
-        Router.push('/home');
-      }
-      if (event === 'SIGNED_OUT') {
-        Router.push('/sign_in');
-      }
-    });
-  }, [checkSession]);
-
-  return <Component {...pageProps} />;
+const MyApp: AppType<{ session: any }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 export default trpc.withTRPC(MyApp);
