fb0b456a41d23b5a09b7ebe71301679d52e5e35d
bebeji-nappa <masadecade@gmail.com>
2023-10-14 20:16:59 +0900

    [ci skip] Github ログインボタンのUI調整

--- src/components/templates/SignIn/index.tsx
+++ src/components/templates/SignIn/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from '@emotion/styled';
-
+import Image from 'next/image';
 import { useSignIn } from "./logic";
 
 const Wrapper = styled.div`
@@ -8,18 +8,18 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 
-const Form = styled.form`
-  width: 30%;
-  display: flex;
-  flex-direction: column;
-`;
-
 const GitHubButton = styled.button`
-  background-color: #333;
+  background-color: #24292f;
   color: #fff;
   border: none;
   border-radius: 5px;
   padding: 10px 20px;
+  margin: 20px;
+  width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
 `;
 
 export type SignInInputs = {
@@ -33,7 +33,10 @@ const SignInTemplate = () => {
   return (
     <Wrapper>
       <h1>Sign In</h1>
-      <GitHubButton onClick={githubSignIn}>Sign in of GitHub</GitHubButton>
+      <GitHubButton onClick={githubSignIn}>
+        <Image src="./github-mark-white.svg" alt="GitHub Logo" width={20} height={18} />
+        Sign in of GitHub
+      </GitHubButton>
     </Wrapper>
   );
 }
