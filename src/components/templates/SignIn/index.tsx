b564837210c765bc365352bed1f4232218a86255
bebeji-nappa <masadecade@gmail.com>
2023-10-14 20:55:43 +0900

    ログイン画面の UI 調整

--- src/components/templates/SignIn/index.tsx
+++ src/components/templates/SignIn/index.tsx
@@ -5,7 +5,35 @@ import { useSignIn } from "./logic";
 
 const Wrapper = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100vh;
+  @supports (height: 100svh) {
+    height: 100svh;
+  }
+  width: 100vw;
+`;
+
+const Container = styled.div`
+  display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 50vw;
+`;
+
+const SigninImage = styled.div`
+  position: relative;
+  width: 50vw;
+  height: 100vh;
+  background-image: url('/images/sign-in-background.jpg');
+  background-size: cover;
+  backgroud-repeat: no-repeat;
+  content-fit: cover;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
 `;
 
 const GitHubButton = styled.button`
@@ -32,11 +60,19 @@ const SignInTemplate = () => {
 
   return (
     <Wrapper>
-      <h1>Sign In</h1>
-      <GitHubButton onClick={githubSignIn}>
-        <Image src="./github-mark-white.svg" alt="GitHub Logo" width={20} height={18} />
-        Sign in of GitHub
-      </GitHubButton>
+      <SigninImage />
+      <Container>
+        <Title>Sign In</Title>
+        <GitHubButton onClick={githubSignIn}>
+          <Image
+            src="/images/github-mark-white.svg"
+            alt="GitHub Logo"
+            width={20}
+            height={18}
+          />
+          Sign in of GitHub
+        </GitHubButton>
+      </Container>
     </Wrapper>
   );
 }
