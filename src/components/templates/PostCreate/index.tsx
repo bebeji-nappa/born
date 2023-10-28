a37bafb59ac9b42826283bb69d38b4376f84d124
bebeji-nappa <masadecade@gmail.com>
2023-10-28 10:27:15 +0900

    [ci skip] コメントアウト削除

--- src/components/templates/PostCreate/index.tsx
+++ src/components/templates/PostCreate/index.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { Richmd } from 'richmd-react';
 import 'richmd/richmd.css';
-// import useStore from '@/store';
 
 const Textarea = styled.textarea`
   width: 100%;
@@ -17,7 +16,6 @@ const StyledRichmd = styled(Richmd)`
 const PostCreateTemplate = () => {
   const [text, setText] = useState('# Hello World');
   const [isEdit, setIsEdit] = useState(true);
-  // const user = useStore((state) => state.user);
 
   return (
     <>
