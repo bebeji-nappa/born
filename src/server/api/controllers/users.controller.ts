5babc04e2ed0b293062c347bb9a8788a8ce93f07
bebeji-nappa <masadecade@gmail.com>
2024-03-03 14:50:46 +0900

    コメント追加

--- src/server/api/controllers/users.controller.ts
+++ src/server/api/controllers/users.controller.ts
@@ -1,5 +1,8 @@
 import { getAll, getAuthUserId } from '../services/users.service';
 
+/**
+ * @get /api/users
+ */
 export const getAllUsersHandler = async () => await getAll();
 
 export const getAuthUserIdHandler = async (email: string) =>
