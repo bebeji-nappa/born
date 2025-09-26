d547e9b4b1652e31920c8e42ccead8029bdef2a1
bebeji-nappa <masadecade@gmail.com>
2025-09-27 01:19:52 +0900

    fix: OGP を1つの画像で統一させる

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const ogImageUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/opengraph-image`;
+
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <title>nappa's Blog</title>
+        <meta
+          property="og:url"
+          content={`${process.env.NEXT_PUBLIC_BASE_URL}`}
+        />
+        <meta property="og:type" content="website" />
+        <meta name="description" content="nappa のブログ。" />
+        <meta property="og:image" content={ogImageUrl} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={ogImageUrl} />
+        <meta name="twitter:title" content="nappa's Blog" />
+        <meta name="twitter:description" content="nappa のブログ。" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,27 +25,7 @@ const Posts = () => {
 
   const ogImageUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/opengraph-image`;
 
-  return (
-    <>
-      <Head>
-        <title>nappa's Blog</title>
-        <meta
-          property="og:url"
-          content={`${process.env.NEXT_PUBLIC_BASE_URL}`}
-        />
-        <meta property="og:type" content="website" />
-        <meta name="description" content="nappa のブログ。" />
-        <meta property="og:image" content={ogImageUrl} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={ogImageUrl} />
-        <meta name="twitter:title" content="nappa's Blog" />
-        <meta name="twitter:description" content="nappa のブログ。" />
-      </Head>
-      <PostListTemplate posts={data} />
-    </>
-  );
+  return <PostListTemplate posts={data} />;
 };
 
 export default Posts;
