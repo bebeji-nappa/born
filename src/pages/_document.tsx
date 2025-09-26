c187378d40f8971499938ffd538e7ba81286b13c
bebeji-nappa <masadecade@gmail.com>
2025-09-26 20:26:49 +0900

    fix: document.tsx の meta を削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,19 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head>
-        <meta
-          property="og:image"
-          content={`${process.env.NEXT_PUBLIC_BASE_URL}/api/og`}
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:image"
-          content={`${process.env.NEXT_PUBLIC_BASE_URL}/api/og`}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
