0b92962f6efbdf282fdd0a6895d625185f53b92e
bebeji-nappa <masadecade@gmail.com>
2025-09-26 16:14:39 +0900

    fix: prismaClient を修正

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from '../../prisma/generated/prisma';
-// import { withAccelerate } from '@prisma/extension-accelerate'
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-// export const prisma = new PrismaClient().$extends(withAccelerate())
-export const prisma = new PrismaClient();
+const prismaClient = new PrismaClient();
+
+export const prisma =
+  process.env.NODE_ENV === 'production'
+    ? prismaClient.$extends(withAccelerate())
+    : prismaClient;
