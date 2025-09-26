8cbbd181b0011b959fd47604ec24f2f0bd30d6ed
bebeji-nappa <masadecade@gmail.com>
2025-09-26 21:00:30 +0900

    fix: メタデータの情報を統一する

--- src/pages/post/[id]/index.tsx
+++ src/pages/post/[id]/index.tsx
@@ -28,19 +28,10 @@ const PostDetail = () => {
     <div>
       <Head>
         <title>{post.title}</title>
-        <meta name="description" content="" />
-        <meta property="og:title" content={post.title} />
-        <meta property="og:description" content="" />
-        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image" content={ogImageUrl} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={post.title} />
-        <meta name="twitter:description" content="" />
-        <meta name="twitter:image:type" content="image/png" />
-        <meta name="twitter:image:width" content="1200" />
-        <meta name="twitter:image:height" content="630" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
       <PostDetailTemplate post={post} />
