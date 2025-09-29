f38907e220197640448e155a9c1bc6986ea341b9
bebeji-nappa <masadecade@gmail.com>
2025-09-29 23:13:41 +0900

    fix: DB 接続設定を修正

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,16 +1,14 @@
-import { PrismaClient } from '../../prisma/generated/prisma'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 export function getPrismaClient(env: any): PrismaClient {
   if (!env?.DATABASE_URL) {
     throw new Error('DATABASE_URL is not defined in environment variables')
   }
   
-  const pool = new Pool({ connectionString: env.DATABASE_URL })
-  const adapter = new PrismaPg(pool)
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+  }).$extends(withAccelerate())
   
-  return new PrismaClient({
-    adapter,
-  })
+  return prisma
 }
