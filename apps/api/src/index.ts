cfca6ebeab3b0f1279032b26d46f193352c8bcbc
bebeji-nappa <masadecade@gmail.com>
2025-10-04 17:19:36 +0900

    fix: ドメイン変更

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -25,7 +25,6 @@ app.use('*', cors({
     const allowedOrigins = [
       'http://localhost:3000',
       'http://127.0.0.1:3000',
-      'https://blog.bebeji-nappa.com',
       'https://staging-blog.bebeji-nappa.com',
       frontendUrl
     ]
